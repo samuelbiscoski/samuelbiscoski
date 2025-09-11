b419e5899a48085cea4a042d7eab2470dfb23dc4
samuelbiscoski <samuel.biscoski.silva@escola.pr.gov.br>
2025-09-11 09:07:12 -0300

    ok

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ const reiniciarBtn = document.getElementById('btn-reiniciar');
 avanca.forEach(button =>{
   button.addEventListener('click',function() {
     const atual = document.querySelector('.ativo');
-    const proximoPasso = 'passo-'+this.getAttribute('data-proximo');
+    const proximoPasso = 'passo-'+this.getAttribute('data-proximo');0
